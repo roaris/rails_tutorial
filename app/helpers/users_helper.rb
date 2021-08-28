c3ca41a67df2b2617180b1c6e4436903b1d887ba
roaris <one0803kyopro@gmail.com>
2021-08-29 01:15:53 +0900

    gravatorでプロフィール画像を表示

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+  def gravator_for(user)
+    gravator_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravator_url = "https://secure.gravatar.com/avatar/#{gravator_id}"
+    image_tag(gravator_url, alt: user.name, class: "gravator")
+  end
 end
