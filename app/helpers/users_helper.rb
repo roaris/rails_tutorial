4cbda11b9ef6b94edaf8bab966e31ce828bc5fda
roaris <one0803kyopro@gmail.com>
2021-08-29 01:21:47 +0900

    プロフィール画像のサイズを変更できるようにした

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
-  def gravator_for(user)
+  def gravator_for(user, size: 80)
     gravator_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravator_url = "https://secure.gravatar.com/avatar/#{gravator_id}"
+    gravator_url = "https://secure.gravatar.com/avatar/#{gravator_id}?s=#{size}"
     image_tag(gravator_url, alt: user.name, class: "gravator")
   end
 end
