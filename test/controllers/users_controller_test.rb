53e6af6d87f14b1f5790325b48a658d92136421a
roaris <one0803kyopro@gmail.com>
2021-08-31 02:18:41 +0900

    admin属性の変更ができないことをテスト

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,6 +1,11 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+    @other_user = users(:archer)
+  end
+
   test "should get new" do
     get signup_path
     assert_response :success
@@ -9,5 +14,18 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should redirect index when not logged in" do
     get users_path
     assert_redirected_to login_url
+  end
+
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: {
+      user: {
+        password: "",
+        password_confirmation: "",
+        admin: true
+      }
+    }
+    assert_not @other_user.reload.admin?
   end
 end
