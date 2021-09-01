1eeb60a96f5f955cab9575b899da97b7181eb360
roaris <one0803kyopro@gmail.com>
2021-09-01 22:54:47 +0900

    有効化処理とメール送信をuser.rbにメソッドとして定義

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -2,12 +2,11 @@ class AccountActivationsController < ApplicationController
   def edit
     user = User.find_by(email: params[:email])
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
-      user.update_attribute(:activated, true)
-      user.update_attribute(:activated_at, Time.zone.now)
+      user.activate
       log_in user
       flash[:success] = "Account activated!"
       redirect_to user
-    elsegi
+    else
       flash[:danger] = "Invalid activation link"
       redirect_to root_url
     end
