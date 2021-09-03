e04b7fd0757542ae31ab841316041dcf89ad393c
roaris <one0803kyopro@gmail.com>
2021-09-04 08:29:16 +0900

    followとunfollowのテストを追加

--- test/controllers/relationships_controller_test.rb
+++ test/controllers/relationships_controller_test.rb
@@ -9,7 +9,7 @@ class RelationshipsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "destroy should require logged-in user" do
-    assert_no difference "Relationship.count" do
+    assert_no_difference "Relationship.count" do
       delete relationship_path(relationships(:one))
     end
     assert_redirected_to login_url
